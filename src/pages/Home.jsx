54733040d61d9389b60d8148ad7be61bc9f8a4bd
DefanZp <defanzainp@gmail.com>
2025-02-18 20:37:02 +0700

    six

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,111 @@
-import React from 'react'
-import Card from '../Components/Card'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Card from "../Components/Card";
+import Bg from "../assets/Bg.jpg";
+
 const Home = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    return (
-        <>
-        <h1 className="text-2xl pt-4 pl-4 pb-4">Discover restaurant</h1>
-        <Link to={"/search"}>
-        <button className='bg-slate-500 text-white px-4 py-2 ml-4'>
-            Search Restaurant
-        </button>
-        </Link>
-        <Card/>
-        </>
-    )
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          "https://restaurant-api.dicoding.dev/list"
+        );
+        setData(response.data.restaurants);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-export default Home
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (data.length > 0) {
+      const interval = setInterval(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
+      }, 3000);
+
+      return () => clearInterval(interval);
+    }
+  }, [data.length]); 
+
+  return (
+    <>
+      <section className="bg-[#FBFBFB] pt-8 px-52 pb-32">
+        <nav className="flex flex-row mb-14">
+          <img
+            width="50"
+            height="50"
+            src="https://img.icons8.com/ios-filled/50/restaurant--v1.png"
+            alt="restaurant--v1"
+          />
+          <ul className="list-none flex flex-row flex-1 items-center justify-center">
+            <li className="flex flex-row gap-12">
+            <Link to={"/"}>
+            <p>Home</p>
+            </Link>
+              <Link to={"/search"}>
+                <p>Search</p>
+              </Link>
+              <p>About</p>
+            </li>
+          </ul>
+        </nav>
+
+        <h1 className="text-[46px] font-semibold pt-4 pl-4 pb-4 text-black text-center mb-20">
+          Temukan restaurantmu!
+        </h1>
+
+        <div className="flex flex-row gap-10">
+          <div className="w-[50%]">
+            {loading ? (
+              <p className="text-center">Loading...</p>
+            ) : data.length > 0 ? (
+              <img
+                src={`https://restaurant-api.dicoding.dev/images/medium/${data[currentIndex]?.pictureId}`}
+                alt={data[currentIndex]?.name}
+                className="w-[600px] h-[350px] object-cover rounded-lg shadow-xl transition-opacity duration-500"
+              />
+            ) : (
+              <p className="text-center">Tidak ada data</p>
+            )}
+          </div>
+
+          <div className="flex flex-col w-[50%] justify-center">
+            <p className=" mb-2 text-xl font-semibold"> Mau cari tempat makan enak?</p>
+            <p className="">
+              Di sini, kamu bisa temukan berbagai restoran dengan makanan lezat
+              dan suasana yang pas buat segala momen. Mau makan bareng keluarga,
+              hangout sama teman, atau makan malam romantis? Kami punya
+              rekomendasi yang pas buat kamu. Gak perlu bingung, cukup cari,
+              klik, dan nikmati makanannya. Yuk, cari restoran yang cocok
+              buatmu!
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section
+        className="bg-black bg-opacity-80 bg-blend-darken"
+        style={{
+          backgroundImage: `url(${Bg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <Card />
+      </section>
+    </>
+  );
+};
+
+export default Home;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,59 +3,165 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const SearchRestaurantPage = () => {
-  
-    const [searchParams,setSearchParams]= useSearchParams();
-    const searchQuery = searchParams.get("q");
-    const [restaurant,setRestaurant]= useState([]);
-    const [loading, setLoading]= useState(true);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchQuery = searchParams.get("q");
+  const [restaurant, setRestaurant] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [inputValue, setInputValue] = useState(searchQuery);
 
-    useEffect(() => {
-        const fetchRestaurant = async () => {
-            try {
-                setLoading(true);
-                const response = await axios.get(`https://restaurant-api.dicoding.dev/search?q=${searchQuery}`);
-                setRestaurant(response.data.restaurants);
-            }
-            catch (error) {
-                console.error("Error fetching data:", error);
-            }
-            finally {
-                setLoading(false);
-            }
-        };
-        
-        fetchRestaurant();
-        }, [searchQuery]);
+  useEffect(() => {
+    const fetchRestaurant = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `https://restaurant-api.dicoding.dev/search?q=${searchQuery}`
+        );
+        setRestaurant(response.data.restaurants);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        const handleSearch = (event) => {
-            setSearchParams({q: event.target.value});
-        }
+    fetchRestaurant();
+  }, [searchQuery]);
+
+  const handleSearch = () => {
+    setSearchParams({ q: inputValue });
+  };
 
   return (
     <>
-    <div className="flex flex-col items-center pt-8">
+      <div className="flex flex-col w-full">
+        <section className="bg-[#FBFBFB] pt-8 px-52 pb-32">
+          <nav className="flex flex-row mb-14">
+            <img
+              width="50"
+              height="50"
+              src="https://img.icons8.com/ios-filled/50/restaurant--v1.png"
+              alt="restaurant--v1"
+            />
+            <ul className="list-none flex flex-row flex-1 items-center justify-center">
+              <li className="flex flex-row gap-12">
+                <Link to={"/"}>
+                  <p>Home</p>
+                </Link>
+                <Link to={"/search"}>
+                  <p>Search</p>
+                </Link>
+                <p>About</p>
+              </li>
+            </ul>
+          </nav>
 
-        <h1>Cari Restoran</h1>
-        <input className="mt-5 mb-5" type="text" placeholder="Cari disini..." value={searchQuery} onChange={handleSearch}>
-        </input>
-        
-        {loading ? (
+          <h1 className="text-[46px] font-semibold pt-4 pl-4 pb-4 text-black text-center mb-20">
+            Cari restaurantmu!
+          </h1>
+
+          <div className="flex flex-row text-[#a8a8a8] items-center shadow-lg justify-between pl-5">
+            <div className="flex flex-row gap-4">
+            <svg 
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2.5}
+              stroke="currentColor"
+              className="size-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
+            </svg>
+
+            <input
+              className=" focus-visible: outline-none"
+              type="text"
+              placeholder="Cari disini..."
+              value={inputValue}
+              onChange={(event) => setInputValue(event.target.value)}
+            ></input>
+
+            </div>  
+
+            <button onClick={handleSearch} className="bg-black px-4 py-4 text-white"> Cari</button>    
+          </div>
+        </section>
+
+        <section>
+        <div className="grid grid-cols-3 gap-y-16 justify-items-center gap-3 overflow-auto px-52 pb-24">    
+          {loading ? (
             <p>Loading...</p>
-        ) : ( searchQuery && (
-            <div className="w-[100%] flex flex-row overflow-x-scroll gap-4">
-                {restaurant.map((restaurants) => (
-                    <Link to={`/restaurant/${restaurants.id}`}>
-                    <div>
-                    <img className="min-w-32" src={`https://restaurant-api.dicoding.dev/images/medium/${restaurants.pictureId}`}></img>    
-                    <h3>{restaurants.name}</h3>
-                    <p>{restaurants.city}</p>
-                    <p>{restaurants.rating}</p>
-                    </div>
+          ) :  ( searchQuery &&(
+                restaurant?.map((item) => (
+                    <Link to={`/restaurant/${item?.id}`} key={item?.id}>
+                      <div
+                        className="flex flex-col justify-between pt-5 pl-5 pb-8 w-64 h-80 bg-cover bg-center bg-black bg-opacity-30 bg-blend-darken rounded-xl"
+                        style={{
+                          backgroundImage: `url(https://restaurant-api.dicoding.dev/images/medium/${item?.pictureId})`,
+                        }}
+                      >
+                        <p className=" bg-black bg-opacity-50 px-6 py-2 rounded-full text-white text-sm w-fit">
+                          Restaurant
+                        </p>
+        
+                        <div className="flex flex-col pl-3">
+                          <p className="text-white text-xl font-semibold mb-2">
+                            {item?.name.length > 20
+                              ? `${item?.name.substring(0, 15)}...`
+                              : item?.name}
+                          </p>
+                          <p className="text-white flex flex-row mb-1 gap-2 text-sm">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke-width="1.5"
+                              stroke="currentColor"
+                              class="size-5"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                              />
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                              />
+                            </svg>
+                            {item?.city}
+                          </p>
+        
+                          <p className="text-white flex flex-row gap-2 text-sm">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="size-5"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                              />
+                            </svg>
+                            {item?.rating}
+                          </p>
+                        </div>
+                      </div>
                     </Link>
-                )) }
-            </div>)
-        )}
-    </div>
+                  )
+            )
+          ))}
+          </div>
+        </section>
+      </div>
     </>
   );
 };
